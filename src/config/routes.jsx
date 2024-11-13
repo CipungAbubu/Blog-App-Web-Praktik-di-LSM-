0d4ab329218922b34fd14f8149f98c730b457a52
CipungAbubu <maryaheba08@gmail.com>
2024-11-13 09:37:54 +0700

    feat: header and footer component

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,31 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home, PostForm, PostDetail } from "../pages";
-import { PostItem } from "../components";
+import { PostItem, MainLayout } from "../components";
 
 export const routes = createBrowserRouter([
     {
         path: '/',
-        element: <Home />
-    },
-    {
-        path: '/post-form',
-        element: <PostForm />
-    },
-    {
-        path: '/post-detail',
-        element: <PostDetail />
-    },
-    {
-        path: '/post-item',
-        element: <PostItem
-            image="/images/photo1.jpg"
-            title="My Favorite K-pop Group"
-            name="NCT DREAM"
-            date="17 Aug 2016"  // Tanggal debut
-            body="NCT DREAM debuted on August 17, 2016, 
-                  and has since captivated fans with 
-                  their energetic performances and music!"
-        />
-    }    
-
-])
+        element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/post-form',
+                element: <PostForm />
+            },
+            {
+                path: '/post-detail',
+                element: <PostDetail />
+            },
+            {
+                path: '/post-item',
+                element: <PostItem
+                    image="/images/photo1.jpg"
+                    title="My Favorite K-pop Group"
+                    name="NCT DREAM"
+                    date="17 Aug 2016" // Tanggal debut
+                    body="NCT DREAM debuted on August 17, 2016, 
+                          and has since captivated fans with 
+                          their energetic performances and music!"
+                />
+            }
+        ]
+    }
+]);
